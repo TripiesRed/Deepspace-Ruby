fff3b90e8aac327967d2d3bf1a7e9c1bd9bda537
TripiesRed <gonzalitoo2spam@gmail.com>
2023-04-16 21:03:33 +0200

    Class SpaceStation

--- DeepSpace/Damage.rb
+++ DeepSpace/Damage.rb
@@ -1,6 +1,11 @@
+module Deepspace
+class Damage
 
 		def getUIversion
 		
 			return DamageToUI.new(self)
 		
 		end
+
+end
+end
--- DeepSpace/SpaceStation.rb
+++ DeepSpace/SpaceStation.rb
@@ -1,5 +1,177 @@
-		def getUIversion
+require_relative 'SpaceStationToUI'
+require_relative 'Damage'
+require_relative 'Loot'
+require_relative 'SuppliesPackage'
+require_relative 'Weapon'
+require_relative 'ShieldBooster'
+require_relative 'Hangar'
+
+module Deepspace
+
+class SpaceStation
+	
+	attr_reader :name, :nMedals, :ammoPower, :fuelUnits, \
+      :shieldPower, :shieldBoosters, :hangar, :weapons, \
+      :pendingDamage
+
+	@@MAXFUEL = 100
+	@@SHIELDLOSSPERUNITSHOT = 0.1
+
+	def initialize (n, supplies ) #Constructor
+
+		@name = n
+		@ammoPower = supplies.ammoPower
+		@fuelUnits = supplies.fuelUnits
+		@shieldPower = supplies.shieldPower
+		@nMedals = nil
+		@pendingDamage = nil
+		@hangar = nil
+		@shieldBoosters = Array.new
+		@weapons = Array.new
+
+	end
+
+	def cleanUpMountedItems
+
+
+
+	end
+
+	def discardHangar
+
+		@hangar = nil
+
+	end
+
+	def discarShieldBooster
+
+	end
+
+	def discarShieldBoosterInHangar
+
+		if(@hangar.nil? == false)
+			@hangar.removeShieldBooster
+		end
+
+	end
+
+	def discardWeapon
+
+	end
+
+	def discardWeaponInHangar
+
+		if(@hangar.nil? == false)
+			@hangar.removeWeapon
+		end
+
+	end
+
+	def fire
+
+	end
+
+	def mountShieldBooster(i)
+
+	end
+
+	def mountWeapon(i)
+
+		@weapons
+
+	end
+
+	def move
+
+	end
+
+	def protection
+
+	end
+
+	def receiveHangar(h)
+
+		if(@hangar.nil == true)
+			@hangar = h
+		end
+
+	end
+
+	def receiveShieldBooster(s)
+
+		if(@hangar.nil? == false)
+			return @hangar.addShieldBooster(s)
 		
-			return SpaceStationToUI.new(self)
+		else return false
 		
 		end
+	end
+
+	def receiveShot(shot)
+
+	end
+
+	def receiveSupplies(s)
+
+		@ammoPower += s.ammoPower
+		@fuelUnits += s.fuelUnits
+		@shieldPower += s.shieldPower
+
+	end
+
+	def receiveWeapon(w)
+
+		if(@hangar.nil? == false)
+			return @hangar.addWeapon(w)
+		
+		else return false
+		
+		end
+
+	end
+	
+	def setLoot(loot)
+
+	end
+
+	def setPendingDamage(d)
+
+		@pendingDamage = d.adjust(@weapons, @shieldBoosters)
+
+	end
+
+	def validState
+
+	end
+	
+	def getUIversion
+	
+		return SpaceStationToUI.new(self)
+	
+	end
+
+	#Métodos Privados
+	private
+
+	def assignFuelValue (f)
+		
+		if(f < @@MAXFUEL)
+			@fuelUnits = f
+		end
+
+	end
+
+	def cleanPendingDamage
+
+		if(@pendingDamage.hasNoEffect)
+			@pendingDamage = nil
+		end
+
+	end
+
+end
+
+end
+
+
+
