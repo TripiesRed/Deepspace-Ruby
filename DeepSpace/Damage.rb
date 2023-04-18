c4e49a71e071a2209a7a006de56bbcf0084ba7e5
diegoos03 <diorsa03@gmail.com>
2023-04-18 19:09:18 +0200

    Clases Ruby

--- DeepSpace/Damage.rb
+++ DeepSpace/Damage.rb
@@ -1,11 +1,86 @@
 module Deepspace
-class Damage
 
-		def getUIversion
-		
-			return DamageToUI.new(self)
-		
+	class Damage
+	  
+	  attr_reader :n_shields, :n_weapons, :weapons
+	  
+	  # Constructor
+	  def initialize(shields, weapons)
+		@n_shields = shields
+		@n_weapons = weapons
+		@weapons = []
+	  end
+	  
+	  def initialize_copy(orig)
+		@n_shields = orig.n_shields
+		@n_weapons = orig.n_weapons
+		@weapons = orig.weapons.clone
+	  end
+	  
+	  # Métodos Visibilidad de Paquete
+	  def get_UIversion
+		DamageToUI.new(self)
+	  end
+	  
+	  # Métodos Privados
+	  def arrayContainsType(w, t)
+		index = -1
+		if w.include?(t)
+		  index = w.index(t)
 		end
-
-end
-end
+		return index
+	  end
+	  
+	  # Métodods Públicos
+	  def adjust(w, s)
+		new_n_weapons, new_n_shields = 0, 0
+		
+		# Ajustamos nWeapons
+		if @n_weapons > w.length
+		  new_n_weapons = w.length
+		else
+		  new_n_weapons = @n_weapons
+		end
+		 
+		# Ajustamos nShields
+		if @n_shields > s.length
+		  new_n_shields = s.length
+		else
+		  new_n_shields = @n_shields
+		end
+		
+		return Damage.new(new_n_shields, new_n_weapons)
+	  end
+	  
+	  def discard_weapon(w)
+		weapon_type = w.get_type
+		weapon_index = arrayContainsType(@weapons, weapon_type)
+		
+		if !@weapons.empty? && weapon_index != -1
+		  @weapons.delete_at(weapon_index)
+		elsif @n_weapons > 0
+		  @n_weapons -= 1
+		end
+	  end
+	  
+	  def discard_shield_booster
+		if @n_shields > 0
+		  @n_shields -= 1
+		end
+	  end
+	  
+	  def has_no_effect
+		@n_shields == 0 && @n_weapons == 0
+	  end
+	  
+	  def set_weapons(w)
+		@weapons = w.clone
+	  end
+	  
+	  def to_s
+		"Shields: #{@n_shields}, Weapons: #{@n_weapons}, Weapon Types: #{@weapons.to_s}"
+	  end
+  
+	end # class Damage
+  
+  end # module Deepspace  
--- DeepSpace/Hangar.rb
+++ DeepSpace/Hangar.rb
@@ -1,5 +1,68 @@
-		def getUIversion
-		
-			return HangarToUI.new(self)
-		
+module Deepspace
+	class Hangar
+	  
+	  attr_reader :max_elements, :weapons, :shield_boosters
+	  
+	  # Constructores
+	  def initialize(capacity)
+		@max_elements = capacity
+		@weapons = []
+		@shield_boosters = []
+	  end
+	  
+	  def initialize_copy(other)
+		@max_elements = other.max_elements
+		@weapons = other.weapons.clone
+		@shield_boosters = other.shield_boosters.clone
+	  end
+	  
+	  # Métodos con Visibilidad de Paquete
+	  def get_ui_version
+		HangarToUI.new(self)
+	  end
+	  
+	  # Métodos Privados
+	  private def space_available?
+		@max_elements > 0
+	  end
+	  
+	  # Métodos Públicos
+	  def add_weapon(w)
+		if space_available?
+		  @weapons.push(w)
+		  @max_elements -= 1
+		  true
+		else
+		  false
 		end
+	  end
+	  
+	  def add_shield_booster(s)
+		if space_available?
+		  @shield_boosters.push(s)
+		  @max_elements -= 1
+		  true
+		else
+		  false
+		end
+	  end
+	  
+	  def remove_shield_booster(s)
+		if s >= 0 && s < @shield_boosters.size
+		  @shield_boosters.delete_at(s)
+		else
+		  nil
+		end
+	  end
+	  
+	  def remove_weapon(w)
+		if w >= 0 && w < @weapons.size
+		  @weapons.delete_at(w)
+		else
+		  nil
+		end
+	  end
+	  
+	end
+  end
+  
